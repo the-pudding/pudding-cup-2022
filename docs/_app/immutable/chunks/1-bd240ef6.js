f85712098d396073b078752512dc9dc3b760398f
Jan Diehm <jadiehm@gmail.com>
2022-11-03 12:49:11 -0500

    update github pages

---
+++ docs/_app/immutable/chunks/1-bd240ef6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-8d8c780f.js";export{t as component};
--- docs/_app/immutable/chunks/2-0b2fb472.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-5efc8437.js";const e=!0;export{t as component,e as server};
